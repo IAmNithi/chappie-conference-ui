d21b3901ea20530eb4232460398eba8bda0a95c8
Kalanithi Sethuraman <kalanithi_sas@outlook.com>
2020-04-09 21:17:41 +0530

    Added Login Page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
+import Login from './Components/Login';
 class App extends React.Component {
   render() {
     return (
+      <div>
       <h1>Hello From Chappie</h1>
+      <Login />
+      </div>
     );
   }
 }
